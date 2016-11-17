b1b78e169cd08bd57d502753699c7d7d1b7f6244
zuraruby <leonardorgananca@gmail.com>
2016-11-17 21:12:08 -0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <windows.h>
+#include <Shlwapi.h> 
+
+int main(int argc, char *argv[])
+{	
+		char path[1000];
+		GetModuleFileName( NULL, path, 1000 );
+		PathRemoveFileSpecA(path);
+	if (argc == 4){
+		char buffer[10000];
+		sprintf(buffer,"echo set PATH=^%%PATH^%%;%s > %s\\%s.bat\n",argv[1],path,argv[3]);
+		system(buffer);
+		sprintf(buffer,"echo %s %%* >> %s\\%s.bat",argv[2],path,argv[3]);
+		system(buffer);
+		printf("Batch file (hopefully) created\n");
+	}else if (argc == 3){
+		char buffer[10000];
+		sprintf(buffer,"echo %s %%* > %s\\%s.bat",argv[1],path,argv[2]);
+		system(buffer);
+		printf("Batch file (hopefully) created\n");
+	}else{
+		printf("Usage: pathalias <path> <executable> <alias> \n");
+		printf("This will create a batch file that adds <path> to a temporary path, and run the <executable> command, tha file name will be <alias>.bat\n");
+		printf("Exemple: pathalias c:\\soft\\java8 java java8 \n\n");
+		printf("Usage: pathalias <path\\executable> <alias>\n");
+		printf("This will create a batch file that runs the <path\\executable>, tha file name will be <alias>.bat\n");
+		printf("Exemple: pathalias c:\\soft\\java8\\java java8");
+	}
+    return 0;
+}
+
